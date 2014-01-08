9806c595cbd9323b8ce46c371f1ec231528d21ce
unknown <gopi.goteti@gmail.com>
2014-01-08 14:11:24 -0800

    working code. download code modified to account for 2013's provisional data. analysis code revised to make graphics using ggplot.

--- analyze_data.R
+++ analyze_data.R
@@ -1,25 +1,159 @@
 # analyze monthly data downloaded from PRISM
+# estimate 2013 total as a percentile of 1895-2012 data
 
 library(rgdal)
 
-# function to rotate a matrix 90 degress clockwise for plotting only
-# used to counteract the "image" function default behavior
+# prism data dimensions
+prism_cols <- 1405
+prism_rows <- 621
+
+# get annual totals for each grid, from 1895 to 2012
+beg_year <- 1895
+end_year <- 2012
+out_data <- array(data = NA, dim = c(prism_rows, prism_cols, length(seq(beg_year, end_year))))
+for (eachYear in seq(beg_year, end_year)) {
+
+  # read data
+  prism_data <- readGDAL(paste0("data/PRISM_ppt_stable_4kmM2_", eachYear, "_bil.bil"),
+                         silent = TRUE)
+  
+  # check data dimensions
+  stopifnot(prism_data@grid@cells.dim[1] == prism_cols)
+  stopifnot(prism_data@grid@cells.dim[2] == prism_rows)
+  
+  # reshape the data to a lat-lon matrix
+  ann_tot <- prism_data@data$band1
+  ann_tot <- matrix(ann_tot, nrow = prism_rows, ncol = prism_cols, byrow = TRUE)
+  
+  # update ouput
+  out_data[, , eachYear - beg_year + 1] <- ann_tot
+}
+
+# monthly totals for 2013
+out_2013 <- array(data = NA, dim = c(prism_rows, prism_cols, 12))
+for (eachMonth in c(1:12)) {
+  
+  # read data
+  if (eachMonth <= 6) {
+    prism_data <- readGDAL(paste0("data/PRISM_ppt_stable_4kmM2_2013", 
+                                  sprintf("%.2d", eachMonth), 
+                                  "_bil.bil"),
+                           silent = TRUE)
+  } else {
+    prism_data <- readGDAL(paste0("data/PRISM_ppt_provisional_4kmM2_2013", 
+                                  sprintf("%.2d", eachMonth), 
+                                  "_bil.bil"),
+                           silent = TRUE)    
+  }
+  
+  # check data dimensions
+  stopifnot(prism_data@grid@cells.dim[1] == prism_cols)
+  stopifnot(prism_data@grid@cells.dim[2] == prism_rows)
+  
+  # reshape the data to a lat-lon matrix
+  mon_tot <- prism_data@data$band1
+  mon_tot <- matrix(mon_tot, nrow = prism_rows, ncol = prism_cols, byrow = TRUE)
+  
+  # update ouput
+  out_2013[, , eachMonth] <- mon_tot
+}
+
+# compute annual total for 2013
+tot_2013 <- apply(out_2013, MARGIN = c(1, 2), FUN = sum, na.rm = TRUE)
+
+# compute historical mean, 1895-2012
+mean_hist <- apply(out_data, MARGIN = c(1, 2), FUN = mean, na.rm = TRUE)
+
+# compute historical mean, 1983-2012
+mean_hist_recent <- apply(out_data[, , c(89:118)], MARGIN = c(1, 2), FUN = mean, na.rm = TRUE)
+
+
+# some graphics
+
+library(ggplot2)
+library(reshape2)
+library(gridExtra) # for plotting multiple panels within a page
+
+# function to rotate a matrix 90 degress clockwise 
+# for plotting only - used to counteract the "image" function default behavior
 Fn_Rotate_Matrix <- function(mat) {
   return (t(mat)[, nrow(mat):1])
 }
 
-# read data and data dimensions
-prism_data <- readGDAL("data/PRISM_ppt_stable_4kmM2_1895_bil.bil")
+# for visualization discretize the color scale
+color_breaks <- c(0, 100, 500, 1000, 1500, 2000, 3000, 4000, 6000, 9000)
+color_labels <- c(100, 500, 1000, 1500, 2000, 3000, 4000, 6000, 9000)
 
-prism_cols <- prism_data@grid@cells.dim[1]
-prism_rows <- prism_data@grid@cells.dim[2]
+Fn_Discretize_Rainfall <- function(in_mat) {
+  in_mat <- Fn_Rotate_Matrix(in_mat)
+  in_mat[in_mat <= 0] <- NA
+  in_mat <- melt(in_mat)
+  in_mat$colorScale <- cut(in_mat$value, 
+                          breaks = color_breaks, 
+                          labels = color_labels,
+                          include.lowest = TRUE)
+  
+  return (in_mat)
+}
 
-# reshape the data for a lat-lon matrix
-ann_tot <- prism_data@data$band1
-ann_tot <- matrix(ann_tot, nrow = prism_rows, ncol = prism_cols, byrow = TRUE)
+# plot 2013 totals
 
-# plot data
-# image(ann_tot)
-image(Fn_Rotate_Matrix(ann_tot))
+tot_2013_gfx <- Fn_Discretize_Rainfall(tot_2013)
 
+gfx_2013 <- ggplot(data = tot_2013_gfx)
+gfx_2013 <- gfx_2013 + geom_raster(aes(Var1, Var2, fill = colorScale))
+gfx_2013 <- gfx_2013 + scale_fill_brewer(palette = "RdYlBu", drop = FALSE)
+gfx_2013 <- gfx_2013 + theme(axis.text = element_blank(), axis.ticks = element_blank())
+gfx_2013 <- gfx_2013 + labs(x = NULL, y = NULL, fill = "Rain (mm/yr)") 
+
+print(gfx_2013)
+
+# plot mean for 1895-2012
+mean_hist_gfx <- Fn_Discretize_Rainfall(mean_hist)
+
+gfx_mean <- ggplot(data = mean_hist_gfx)
+gfx_mean <- gfx_mean + geom_raster(aes(Var1, Var2, fill = colorScale))
+gfx_mean <- gfx_mean + scale_fill_brewer(palette = "RdYlBu", drop = FALSE)
+gfx_mean <- gfx_mean + theme(axis.text = element_blank(), axis.ticks = element_blank())
+gfx_mean <- gfx_mean + labs(x = NULL, y = NULL, fill = "Rain (mm/yr)") 
+
+print(gfx_mean)
+
+
+# plot 2013 total as fraction of mean
+Fn_Discretize_Fraction <- function(in_mat) {
+  in_mat <- Fn_Rotate_Matrix(in_mat)
+  in_mat[in_mat <= 0] <- NA
+  in_mat <- melt(in_mat)
+  in_mat$colorScale <- cut(in_mat$value, 
+                           breaks = c(0, 0.1, 0.2, 0.3, 0.5, 0.75, 1, 1.5, 3), 
+                           labels = c("10%", "20%", "30%", "50%", "75%", "100%", "150%", "300%"),
+                           include.lowest = TRUE)
+  
+  return (in_mat)
+}
+
+# w.r.t. long term average
+frac_2013 <- tot_2013 / mean_hist
+frac_2013 <- Fn_Discretize_Fraction(frac_2013)
+
+gfx_frac <- ggplot(data = frac_2013)
+gfx_frac <- gfx_frac + geom_raster(aes(Var1, Var2, fill = colorScale))
+gfx_frac <- gfx_frac + scale_fill_brewer(palette = "RdYlBu", drop = FALSE)
+gfx_frac <- gfx_frac + theme(axis.text = element_blank(), axis.ticks = element_blank())
+gfx_frac <- gfx_frac + labs(x = NULL, y = NULL, fill = "% Historical Avg") 
+
+print(gfx_frac)
+
+# w.r.t. past 30-yr average
+frac_2013 <- tot_2013 / mean_hist_recent
+frac_2013 <- Fn_Discretize_Fraction(frac_2013)
+
+gfx_frac <- ggplot(data = frac_2013)
+gfx_frac <- gfx_frac + geom_raster(aes(Var1, Var2, fill = colorScale))
+gfx_frac <- gfx_frac + scale_fill_brewer(palette = "RdYlBu", drop = FALSE)
+gfx_frac <- gfx_frac + theme(axis.text = element_blank(), axis.ticks = element_blank())
+gfx_frac <- gfx_frac + labs(x = NULL, y = NULL, fill = "% Historical Avg") 
+
+print(gfx_frac)
 
--- obtain_data.R
+++ obtain_data.R
@@ -1,15 +1,18 @@
 # obtain data from the PRISM ftp site (prism.nacse.org)
 # see http://prism.oregonstate.edu/documents/PRISM_downloads.pdf
+# 2013 data is still not all "stable" and so not available in one zip file
+# download data for 2013 separately
 
 # folder where all the monthly precip data resides
 base_url <- "ftp://prism.nacse.org/monthly/ppt/"
 
-for (eachYear in c(1895:2013)) {
+# data for 1895 to 2012
+for (eachYear in c(1895:2012)) {
     
   cat("reading...", eachYear, "\n")
   
   # construct final url and output file name
-  year_url <- paste0("PRISM_ppt_stable_4kmM2_", eachYear, "_all_bil.zip")                            
+  year_url <- paste0("PRISM_ppt_stable_4kmM2_", eachYear, "_all_bil.zip")
   prism_url <- paste0(base_url, eachYear, "/", year_url)
   outFile   <- paste0("data/", year_url)
   
@@ -30,10 +33,52 @@ for (eachYear in c(1895:2013)) {
   # only extract the files corresponding to the entire year
   file_names <- unzip(outFile, list = TRUE)
   find_str <- paste0("PRISM_ppt_stable_4kmM2_", eachYear, "_")
-  file_names <- file_names$Name[grep(find_str, file_names$Name)]
-  
+  file_names <- file_names$Name[grep(find_str, file_names$Name)]  
   unzip(outFile, files = file_names, exdir = "data")
   
   # delete raw file
   file.remove(outFile)
+}
+
+# data for 2013
+for (eachYear in c(2013)) {
+  
+  cat("reading...", eachYear, "\n")
+  
+  # construct final url and output file name
+  for (eachMonth in c(1:12)) {
+    if (eachMonth <= 6) {
+      year_url <- paste0("PRISM_ppt_stable_4kmM2_", 
+                         eachYear, 
+                         sprintf("%.2d", eachMonth), 
+                         "_bil.zip")
+    } else {
+      year_url <- paste0("PRISM_ppt_provisional_4kmM2_", 
+                         eachYear, 
+                         sprintf("%.2d", eachMonth), 
+                         "_bil.zip")      
+    }
+    prism_url <- paste0(base_url, eachYear, "/", year_url)
+    outFile   <- paste0("data/", year_url)
+    
+    # internet connection could be "intermittent" and files are not sometimes downloaded
+    # hence, the quieted while loop below; max tries limited to 20 to avoid infinite loops
+    fileError <- TRUE
+    max_tries <- 0
+    while (fileError & max_tries < 20) {
+      max_tries <- max_tries + 1
+      fileStatus <- try(download.file(url = prism_url, 
+                                      destfile = outFile, 
+                                      mode = "wb", 
+                                      quiet = TRUE), 
+                        silent = TRUE)
+      fileError  <- ifelse(class(fileStatus) == "try-error", TRUE, FALSE)
+    }
+    
+    # only extract the files corresponding to the entire year
+    unzip(outFile, exdir = "data")
+    
+    # delete raw file
+    file.remove(outFile)
+  }
 }
